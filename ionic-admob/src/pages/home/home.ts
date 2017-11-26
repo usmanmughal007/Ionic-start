121646833f03a94dd327525816305f01d3ffc9bb
usmanmughal007 <decent.usman143@gmail.com>
2017-11-26 15:06:45 +0500

    Adding new Ionic Project

---
+++ ionic-admob/src/pages/home/home.ts
@@ -0,0 +1,90 @@
+// import { AdMob } from '@ionic-native/status-bar';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  
+  constructor(public navCtrl: NavController 
+    , public admob: AdMobFree
+  ) {
+
+  }
+
+  showBanner() {
+    
+           const bannerConfig: AdMobFreeBannerConfig = {
+               isTesting: true, // Remove in production
+               autoShow: true,
+               //publisherId: admob_key
+              
+               id: 'ca-app-pub-9604915415648306/7098266830'
+           };
+    
+           this.admob.banner.config(bannerConfig);
+    
+            this.admob.banner.prepare().then(() => {
+               // success
+            }).catch(e => console.log(e));
+    
+       }
+
+       
+    launchInterstitial() {
+      
+             let interstitialConfig: AdMobFreeInterstitialConfig = {
+                 isTesting: true, // Remove in production
+                 autoShow: true,
+                 id: 'ca-app-pub-9604915415648306/7098266830'
+             };
+      
+             this.admob.interstitial.config(interstitialConfig);
+      
+             this.admob.interstitial.prepare().then(() => {
+                 // success
+             })
+             .catch(e => console.log(e));
+            }
+
+
+
+            showRewardVideoAds(){
+              const bannerConfig: AdMobFreeBannerConfig = {
+              id: 'ca-app-pub-9604915415648306/7098266830',
+              isTesting: false,
+              autoShow: false
+              };
+              this.admob.rewardVideo.config(bannerConfig);
+          
+              this.admob.rewardVideo.prepare()
+              .then(() => {
+                  this.admob.rewardVideo.show()
+              })
+              .catch(e => console.log(e));    
+              }
+
+
+              // loadAd(){
+              //   let options = {
+              //    adId: 'ca-app-pub-9604915415648306/7098266830',
+              //    isTesting: false
+              //   };
+              //    AdMob.prepareInterstitial(options)
+              //         .then(() => {
+              //           AdMob.showInterstitial();
+              //         });
+              //  };
+
+            showDetais()
+            {
+                
+
+
+            }
+
+}
